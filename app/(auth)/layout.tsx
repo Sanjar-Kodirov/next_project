1e17ff9f2c129fb30bc36e0fe78af7fe481ba61e
sqodirov <sqodirov@cdti.uz>
2024-05-24 11:53:51 +0500

    delete some trash code

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="flex-center h-screen text-white">
-      {children}this is auth
-    </main>
-  );
+  return <main className="flex-center h-screen text-white">{children}</main>;
 };
 
 export default Layout;
