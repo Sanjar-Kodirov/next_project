de68972e7b45b5c663f2bf7e5fd2ef271f7d85fa
sqodirov <sqodirov@cdti.uz>
2024-05-24 11:53:18 +0500

    right sidebar

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="flex-center text-white">{children}this is auth</main>;
+  return (
+    <main className="flex-center h-screen text-white">
+      {children}this is auth
+    </main>
+  );
 };
 
 export default Layout;
